d8309bbacb89462cd5c3b7898d54ebc889486542
Chingis007 <karaulnekerell@gmail.com>
2025-01-07 11:52:44 +0200

    finaly

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
 import { drizzle } from "drizzle-orm/neon-http"
 import { neon } from "@neondatabase/serverless"
-// import { config } from "dotenv"
+import { config } from "dotenv"
 
-// if (process.env.NODE_ENV === "development") {
-//   console.log("loading .env.local")
-//   config({ path: ".env.local" })
-// }
+if (process.env.NODE_ENV === "development") {
+  console.log("loading .env.local")
+  config({ path: ".env.local" })
+}
 
 // const sql = neon(process.env.DATABASE_URL!)
 
